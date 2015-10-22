3e9588de6035e0e4eec6a7295eb2c7abc6582961
Christopher Lloyd <christopher.lloyd@theguardian.com>
2015-10-22 10:16:29 +0100

    Hello World

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var graphqlSchema = require('./schema.js');
+
+var app = express();
+
+app.use('/graphql', graphqlHTTP({ schema: graphqlSchema, graphiql: true }));
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
